46ba91b685ab56166e85f5531f4d5b69efd6288e
Glitch (hello-express) <none>
2020-10-08 18:57:14 +0000

    🐥💼 Checkpoint ./config.js:15367608/748

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
     "token",
     "token",
     "token",
-    "token",
+    "token"
   ],
-  voice_channel: "759234356467466251", 
-  text_channel: "750741284347445258", 
-  your_server: "721019622240944129" 
+  voice_channel: "759234356467466251",
+  text_channel: "750741284347445258",
+  your_server: "721019622240944129"
 };
